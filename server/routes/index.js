076698e02361929405529e528fb6fad2d5170a93
MilkoAnello <anello.milko@gmail.com>
2020-03-04 15:27:34 +0100

    Initial commit

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,18 +7,17 @@
 //
 /////////////////////////////////////////////////////////////////////////////////////////
 
-//var m_Express = require('express');
 var m_Router = require('express').Router();
 
-const m_OracleDB = require('../Services/oracle_db');
+const m_OracleDB = require('../services/oracle_db');
 
 m_Router.get('/', async function(_Req, _Res, _Next) {
 //  _Res.render('index', { title: 'Express' });
-const result = await m_OracleDB.Execute('select user, systimestamp from dual');
-const user = result.rows[0].USER;
-const date = result.rows[0].SYSTIMESTAMP;
+    const result = await m_OracleDB.Execute('select user, sysdate from dual');
+    const user = result.rows[0].USER;
+    const date = result.rows[0].SYSDATE;
 
-_Res.end(`DB user: ${user}\nDate: ${date}`);
+    _Res.end(`DB user: ${user}\nDate: ${date}`);
 });
 
 module.exports = m_Router;
